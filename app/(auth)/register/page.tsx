ce34a298ad50363aa2fec1afd4f5d27423959f5c
Ken Chigozie <kenronkwo@gmail.com>
2024-03-23 14:54:08 +0100

    modified

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -139,7 +139,7 @@ const SignUp = () => {
                 disabled={isPending}
                 type="text"
                 id="username"
-                name="username"
+                
                 placeholder="name"
                 {...register("name")}
                 className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
