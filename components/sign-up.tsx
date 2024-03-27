2840622abd47c2ffb35c1fda033c3cfdd6dd4327
Ken Chigozie <kenronkwo@gmail.com>
2024-03-27 16:59:59 +0100

    modified

--- components/sign-up.tsx
+++ components/sign-up.tsx
@@ -53,13 +53,13 @@ function SigningUp() {
    <div className="flex h-screen">
        
       {/* Left Pane */}
-      <div className="hidden lg:flex items-center justify-center flex-1 bg-white text-black">
+      <div className="hidden lg:flex items-center justify-center flex-1 bg-gray-100 text-black">
         <div className="max-w-md text-center drop-shadow-md">
           <Image src={"/team.svg"} height={300} width={300} alt="img" />
         </div>
       </div>
       {/* Right Pane */}
-      <div className="w-full  bg-gray-100 lg:w-1/2 flex items-center justify-center">
+      <div className="w-full  bg-white shadow-lg rounded lg:w-1/2 flex items-center justify-center">
         <div className="max-w-md w-full p-6">
           <h1 className="text-3xl font-semibold mb-6 text-black text-center">
             Sign Up
